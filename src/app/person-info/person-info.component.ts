9dd431fc1d0e6a654feacd4205e4f82ca136136c
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-03-05 15:24:52 +0200

    Create person component

--- src/app/person-info/person-info.component.ts
+++ src/app/person-info/person-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonInfo, States, Occupations } from './../person';
+import { PersonLogic } from './../person-logic';
 
 @Component({
   selector: 'app-person-info',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonInfoComponent implements OnInit {
 
-  constructor() { }
+  person: PersonInfo;
+  persons: PersonInfo[];
+  logic: PersonLogic;
+  states = States;
+  occupations = Occupations;
+  tableColumns: any[];
 
-  ngOnInit() {
+  constructor() {
+    this.person = new PersonInfo(0, '', '', '', '', '');
+    this.persons = new Array();
+    this.logic = new PersonLogic();
+    this.tableColumns = new Array();
   }
 
+  ngOnInit() {
+    for (let p in this.person) {
+      this.tableColumns.push(p);
+    }
+    this.persons = this.logic.getPersonsInfo();
+  }
+  clear(): void {
+    this.person = new PersonInfo(0, '', '', '', '', '');
+  }
+  save(): void {
+    this.logic.savePersonInfo(this.person);
+  }
+  getSelectedData(p: PersonInfo): void {
+    this.person = Object.assign({}, p);
+  }
 }
