869ce3b75b4abe873af02fbe3486241a69aec697
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-03-05 15:35:33 +0200

    Create basic html form for person addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { PersonInfoComponent } from './person-info/person-info.component';
@@ -10,7 +11,8 @@ import { PersonInfoComponent } from './person-info/person-info.component';
     PersonInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
